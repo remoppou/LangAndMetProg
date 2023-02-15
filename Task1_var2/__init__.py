eb16188a0ce9593c59101086ca8e61c10a4fca21
remoppou <voronezhskij_n_m@sc.vsu.ru>
2023-02-15 18:55:58 +0300

    task1_var2

--- Task1_var2/__init__.py
+++ Task1_var2/__init__.py
@@ -1,6 +1,22 @@
+def read_from_file():
+    with open('input.txt', 'r') as file:
+        return list(map(int, file.readline().split()))
 
 
+def write_to_file(ans_list):
+    with open('output.txt', 'w') as file:
+        for item in ans_list:
+            file.write(format(f"{item} "))
+
+
+def solve_func(l):
+    new_list = []
+    for item in l:
+        if not new_list.__contains__(item):
+            new_list.append(item)
+    return new_list
 
 
 if __name__ == '__main__':
-    print(1)
+    write_to_file(solve_func(read_from_file()))
+
